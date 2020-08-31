3335419b6886364e94da8a7bffaec21e18916f98
MeetFalcon <meetfalconcode@gmail.com>
2020-08-31 12:47:14 +0530

    Endpoint for creating and authenticating users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -26,7 +26,6 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-
     def test_user_change_page(self):
         '''Test that user edit page works'''
         url = reverse('admin:core_user_change', args=[self.user.id])
